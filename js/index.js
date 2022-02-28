eeca5f273fa1971ef355bc6a55efc9ddd3f3a108
Mehedyh259 <mehedyh259@gmail.com>
2022-02-28 23:23:22 +0600

    phone display added

--- js/index.js
+++ js/index.js
@@ -7,18 +7,35 @@ const getPhone = () => {
     // document.getElementById('phones').textContent = '';
     toggleSpinner('block');
     const search = document.getElementById('search-phone').value.toLowerCase();
-
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`)
+    const url = `https://openapi.programming-hero.com/api/phones?search=${search}`;
+    fetch(url)
         .then(res => res.json())
-        .then(data => displayPhones(data.data.slice(0,20)))
-        
+        .then(data => displayPhones(data.data))
+
 }
 
-const displayPhones = phones =>{
-    if(phones.length == 0){
-        console.log('not found')
-    }else{
-        console.log(phones);
+const displayPhones = phones => {
+    const container = document.getElementById('phones');
+    container.textContent='';
+    if (phones.length != 0) {
+        const first20Phones = phones.slice(0, 20);
+        first20Phones.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('col-12', 'col-sm-6', 'col-md-4', 'col-lg-4', 'my-2');
+            div.innerHTML = `
+                <div class="card bg-light">
+                    <img src=${phone.image} style="width: 100%;" class="card-img-top img-thumbnail" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">Brand: ${phone.brand}</h5>
+                        <small class="card-text">Model: ${phone.phone_name}</small><br>
+                        <button onclick="showDetails('${phone.slug}')" class="btn btn-sm btn-primary mt-2">Details</button>
+                    </div>
+                </div> 
+            `;
+            container.appendChild(div);
+        });
+    } else {
+        console.log('error');
     }
     toggleSpinner('none');
 }
